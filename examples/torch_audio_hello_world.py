ac8ba83f4664e0a3d87992784ede429ef10f9c06
Mika Laitio <mika.laitio@amd.com>
2025-09-19 16:47:40 +0000

    add torchcodec check to torch audio example
    - torchcodec is new library for torchaudio
      which has both the cpu and cuda backends
    
    Signed-off-by: Mika Laitio <mika.laitio@amd.com>

--- examples/torch_audio_hello_world.py
+++ examples/torch_audio_hello_world.py
@@ -3,3 +3,9 @@ import torchaudio
 
 print("pytorch version: " + torch.__version__)
 print("pytorch audio version: " + torchaudio.__version__)
+
+try:
+    import torchcodec
+    print("torchcodec version: " + torchcodec.__version__)
+except ImportError:
+    print("Could not find torchcodec module")
