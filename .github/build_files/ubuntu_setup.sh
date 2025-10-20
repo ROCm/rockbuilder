ebba2dc5591d2b67baee763c3e5adc84e668dcb9
Mika Laitio <mika.laitio@amd.com>
2025-10-19 21:24:52 -0700

    add github action to build python wheels
    - add simple github action to build therock
      and then a set of apps.
    
    Signed-off-by: Mika Laitio <mika.laitio@amd.com>

---
+++ .github/build_files/ubuntu_setup.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install gfortran git git-lfs ninja-build cmake g++ pkg-config xxd patchelf automake libtool python3-venv python3-dev libegl1-mesa-dev
+
+git config --global user.email "rockbuilder@amd.com"
+git config --global user.name "rockbuilder"
