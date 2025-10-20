3f86671e44a4df485548043655c9ca1bb3d02ccb
Mika Laitio <mika.laitio@amd.com>
2025-10-19 21:24:52 -0700

    add github action to build python wheels
    - add simple github action to build therock
      and then a set of apps.
    
    Signed-off-by: Mika Laitio <mika.laitio@amd.com>

---
+++ .github/build_files/init_and_build_apps.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+cd ../..
+
+export LC_ALL=C
+sudo dpkg-reconfigure locales
+
+if [[ -n "$VIRTUAL_ENV" ]]; then
+    echo "Virtual environment is active: $VIRTUAL_ENV"
+else
+     echo "No virtual environment is active."
+     source ./init_rcb_env.sh
+fi
+
+./rockbuilder.py apps/therock.cfg
+if [ ! $? -eq 0 ]; then
+    echo ""
+    echo "Failed to build therock"
+    exit 1
+fi
+
+./rockbuilder.py apps/pytorch_28_amd.apps
+if [ ! $? -eq 0 ]; then
+    echo ""
+    echo "Failed to build apps/pytorch_28_amd.apps"
+    exit 1
+fi
+
+
--- init_rcb_env.sh
+++ init_rcb_env.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+VENV_NAME=.venv
 LAUNCH_DIR=$(pwd)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
+
+echo "The current directory is: $LAUNCH_DIR"
+echo "Script directory is: $SCRIPT_DIR"
 
 # Check that this script is called as sourced.
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
@@ -12,11 +16,20 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     exit 1
 fi
 
-if [ -f ./.venv/bin/activate ]; then
-    source .venv/bin/activate
+if [ ! -z "$VIRTUAL_ENV" ]; then
+    echo "Python virtual environment used: $VIRTUAL_ENV"
 else
-    python3 -m venv .venv && source .venv/bin/activate
-    pip install --upgrade pip
-    pip install -r requirements.txt
+    if [ ! -f ./.venv/bin/activate ]; then
+        echo "Creating python virtual environment: $VENV_NAME"
+        python3 -m venv .venv
+        echo "Activating python virtual environment: $VENV_NAME"
+        source .venv/bin/activate
+        pip install --upgrade pip
+        pip install -r requirements.txt
+        echo "Python virtual environment created and enabled: $VIRTUAL_ENV"
+    else
+        source .venv/bin/activate
+        echo "Python virtual environment enabled: $VIRTUAL_ENV"
+    fi
 fi
 cd ${LAUNCH_DIR}
