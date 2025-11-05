e1e3addc75d9fafbcdc756ce1bfd471452d4cbcb
Mika Laitio <lamikr@gmail.com>
2025-11-05 15:44:06 -0800

    cleanup torch attention test code
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- examples/torch_attention_check.py
+++ examples/torch_attention_check.py
@@ -4,9 +4,6 @@ import torch.nn.functional as F
 import torch.utils.benchmark as benchmark
 from torch.nn.attention import SDPBackend, sdpa_kernel
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-#device = "cpu"
-
 # Example Usage:
 # query = torch.randn(2, 3, 8, device=device)
 # key = torch.randn(2, 3, 8, device=device)
@@ -15,116 +12,130 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def benchmark_torch_ms(f, *args, **kwargs):
     t0 = benchmark.Timer(
-        stmt="f(*args, **kwargs)", globals={"args": args, "kwargs": kwargs, "f": f}
+        stmt = "f(*args, **kwargs)", globals={"args": args, "kwargs": kwargs, "f": f}
     )
-    return t0.blocked_autorange().mean * 1e6
+    # get execution time
+    ret = t0.blocked_autorange().mean * 1e6
+    return ret
 
 
-def exec_test(is_enabled, name, val_default):
+def exec_test(is_enabled,
+              name,
+              val_reference,
+              query,
+              key,
+              value):
     ret = None
+
     if is_enabled:
         try:
+            # first run of function to get the execution time
             exec_ms = benchmark_torch_ms(F.scaled_dot_product_attention, query, key, value)
-            print(f"    {name} exec time: {exec_ms:.3f} ms")
+            # second run of function to get the value from the executed function
             ret = F.scaled_dot_product_attention(query, key, value)
-            # Verify that the outputs are close (they should be, as MATH is a correct implementation)
-            if val_default is None:
-                print("val_default: None")
+            if ret is not None:
+                if val_reference is not None:
+                    # Verify that the result is close to reference_value calculated with SDPBackend.Math
+                    try:
+                        torch.testing.assert_close(ret, val_reference)
+                        print(f"    success:   value matches with the reference value from SDPBackend.Math")
+                        print(f"    exec time: {exec_ms:.3f} ms")
+                        print(f"    res.shape: {ret.shape}")
+                    except AssertionError as e:
+                        print(f"{name} error, incorrect value for scaled_dot_product_attention: {e}")
+                        for row in ret:
+                            print(row)
+                else:
+                    print(f"    success:   reference value calculated with SDPBackend.Math")
+                    print(f"    exec time: {exec_ms:.3f} ms")
+                    print(f"    res.shape: {ret.shape}")
             else:
-                print("val_default: not None")
-            if ret is not None and val_default is not None:
-                print("not none, shape: ", ret.shape)
-                try:
-                    torch.testing.assert_close(ret, val_default)
-                    print("Results are mathematically equivalent. The backends work as expected.")
-                    for row in ret:
-                        print(row)
-                except AssertionError as e:
-                    print(f"Assertion failed: {e}")
-                #if torch.eq(val_default, ret):
-                #    print("Outputs are same")
-                #else:
-                #    print("Outputs are different")
+                print(f"{name} error, failed to calculate scaled_dot_product_attention")
         except RuntimeError as ex:
             print(f"{name} error: {ex}")
     else:
         print(f"{name} not enabled")
     return ret
 
+def benchmark_sdb_backend(device:str, dtype):
+    val_reference = None
+    # hyper-parameters
+    batch_size = 16
+    num_heads = 16
+    max_sequence_len = 64
+    embed_dimension = 128
 
-# Lets define the hyper-parameters of our input
-batch_size = 16
-num_heads = 16
-max_sequence_len = 64
-embed_dimension = 128
-
-dtype = torch.float16
-
-query = torch.rand(batch_size,
+    query = torch.rand(batch_size,
                    num_heads,
                    max_sequence_len,
                    embed_dimension,
                    device=device,
                    dtype=dtype)
-key = torch.rand(batch_size,
+    key = torch.rand(batch_size,
                  num_heads,
                  max_sequence_len,
                  embed_dimension,
                  device=device,
                  dtype=dtype)
-value = torch.rand(batch_size,
+    value = torch.rand(batch_size,
                    num_heads,
                    max_sequence_len,
                    embed_dimension,
                    device=device,
                    dtype=dtype)
-F.scaled_dot_product_attention(query, key, value)
+    F.scaled_dot_product_attention(query, key, value)
 
-val_default = None
+    with sdpa_kernel(SDPBackend.MATH):
+        name = str(SDPBackend.MATH)
+        print("----------")
+        print(name + " test started")
+        try:
+            is_enabled = torch.backends.cuda.math_sdp_enabled()
+            val_reference = exec_test(is_enabled, name, None, query, key, value)
+        except RuntimeError as ex:
+            print(f"{name} error support check failed: {ex}")
+        print(name + " test done")
+    
+    with sdpa_kernel(SDPBackend.EFFICIENT_ATTENTION):
+        name = str(SDPBackend.EFFICIENT_ATTENTION)
+        print("----------")
+        print(name + " test started")
+        try:
+            is_enabled = torch.backends.cuda.mem_efficient_sdp_enabled()
+            exec_test(is_enabled, name, val_reference, query, key, value)
+        except RuntimeError as ex:
+            print(f"{name} error support check failed: {ex}")
+        print(name + " test done")
+    
+    
+    with sdpa_kernel(SDPBackend.FLASH_ATTENTION):
+        name = str(SDPBackend.FLASH_ATTENTION)
+        print("----------")
+        print(name + " test started")
+        try:
+            is_enabled = torch.backends.cuda.flash_sdp_enabled()
+            exec_test(is_enabled, name, val_reference, query, key, value)
+        except RuntimeError as ex:
+            print(f"{name} error support check failed: {ex}")
+        print(name + " test done")
 
-name = "SDPBackend.Default"
-print("----------")
-print(name + " test started")
-try:
-    is_enabled = torch.backends.cuda.math_sdp_enabled()
-    val_default = exec_test(is_enabled, name, None)
-    print("not none, shape: ", val_default.shape)
-except RuntimeError as ex:
-    print(f"{name} error support check failed: {ex}")
-print(name + " test done")
-
-with sdpa_kernel(SDPBackend.MATH):
-    name = "SDPBackend.MATH"
+    name = "SDPBackend.Default"
     print("----------")
     print(name + " test started")
     try:
         is_enabled = torch.backends.cuda.math_sdp_enabled()
-        exec_test(is_enabled, name, val_default)
+        exec_test(is_enabled, name, val_reference, query, key, value)
     except RuntimeError as ex:
         print(f"{name} error support check failed: {ex}")
     print(name + " test done")
 
-"""
-with sdpa_kernel(SDPBackend.EFFICIENT_ATTENTION):
-    name = "SDPBackend.EFFICIENT_ATTENTION"
-    print("----------")
-    print(name + " test started")
-    try:
-        is_enabled = torch.backends.cuda.mem_efficient_sdp_enabled()
-        exec_test(is_enabled, name, val_default)
-    except RuntimeError as ex:
-        print(f"{name} error support check failed: {ex}")
-    print(name + " test done")
-"""
 
-with sdpa_kernel(SDPBackend.FLASH_ATTENTION):
-    name = "SDPBackend.FLASH_ATTENTION"
-    print("----------")
-    print(name + " test started")
-    try:
-        is_enabled = torch.backends.cuda.flash_sdp_enabled()
-        exec_test(is_enabled, name, val_default)
-    except RuntimeError as ex:
-        print(f"{name} error support check failed: {ex}")
-    print(name + " test done")
+def main():
+    dtype = torch.float16
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    benchmark_sdb_backend(device, dtype)
 
+if __name__ == "__main__":
+    main()
